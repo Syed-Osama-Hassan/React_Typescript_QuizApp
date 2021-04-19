61997696be15c5995df220e0b49bd4d98747dc05
syed-osama-hassan <syedosamahassan16@gmail.com>
2021-04-20 02:27:26 +0500

    complete quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions } from './API';
+import { QuestionState, Difficulty } from './API';
+import './App.css';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions,setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver){
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      
+      if(correct) setScore(prev => prev + 1);
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject])
+    }
+  }
+
+  const nextQuestion = () => {
+    //Move to next question if not last question
+    const nextQuestion = number + 1;
+    if(nextQuestion === TOTAL_QUESTIONS){
+      setGameOver(true);
+    }
+    else{
+      setNumber(nextQuestion);
+    }
+  }
+
   return (
     <div className="App">
-      <h2>Quiz</h2>
+      <h1 className="heading">Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ?(
+        <button className="btn btn-outline-primary start" onClick={startQuiz}>Start</button>
+
+      ): null }
+      {!gameOver? <p className="score">Score: {score}</p>: null}
+      {loading && <div className="spinner-border text-secondary">
+        <p className="visually-hidden">Loading...</p>
+      </div>}
+      {!loading && !gameOver && (
+        <QuestionCard 
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers = {questions[number].answers}
+        userAnswer = {userAnswers? userAnswers[number] : undefined}
+        callback= {checkAnswer}
+      />)}
+
+      {!loading && !gameOver && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ?
+        (<button className="btn btn-primary next col-2 justify-content-md-end" onClick={nextQuestion}>
+        Next
+      </button>): null
+      }
     </div>
   );
 }
